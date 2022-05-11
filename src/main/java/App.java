9a4dc10a23ab1369c3779629ef6ac3008069daa5
jumaBrian <njengabrianj@gmail.com>
2022-05-11 21:17:44 +0300

    doc: Add handlebars layout

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,26 @@
+import Models.Endangered;
+import Models.Safe;
+import Models.Sightings;
 
 import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
 
     public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
+        // home page
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
     }
 }
