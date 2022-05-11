5b4913f9fc8cfe461f18fd6ec6649d6079e0db24
jumaBrian <njengabrianj@gmail.com>
2022-05-11 23:33:54 +0300

    ft: Return animals record

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,7 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class App {
@@ -26,7 +27,39 @@ public class App {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "animals.hbs");
         }, new HandlebarsTemplateEngine());
+/*
+        // display animals on record
+        post("/AnimalDetail", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            String type = request.queryParams("type");
+            String health = request.queryParams("health");
+            String age = request.queryParams("age");
+            model.put("name", name);
+            model.put("type", type);
+            model.put("health", health);
+            model.put("age",age);
 
+            if(type == "safe"){
+                Safe animal = new Safe(name,age,health,type);
+                animal.save();
+            }
+            else {
+                Endangered animal = new Endangered(name,age,health,type);
+                animal.save();
+            }
+            return new ModelAndView(model, "displayAnimals.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/Animals", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            List<Safe> safeAnimal = Safe.all();
+            List<Endangered>  unsafeAnimal = Endangered.all();
+            model.put("safeAnimal", safeAnimal);
+            model.put("unsafeAnimal",unsafeAnimal);
+            return new ModelAndView(model, "allAnimals.hbs");
+        }, new HandlebarsTemplateEngine());
+*/
         // report sightings form
         get("/sightings-form", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
