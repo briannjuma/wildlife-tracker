32559da9e557d5ac3acb7d5cd1546d89a3265003
jumaBrian <njengabrianj@gmail.com>
2022-05-11 23:15:06 +0300

    ft: Add animals from web page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,6 +21,13 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
+        // animals registry
+        get("/animals-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "animals.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
 
     }
 }
