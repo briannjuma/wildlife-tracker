a8eff70dc9366027419dd2d1a03a911c4151a604
jumaBrian <njengabrianj@gmail.com>
2022-05-12 09:49:34 +0300

    ft: Add sightings to database

--- src/main/java/Models/Sightings.java
+++ src/main/java/Models/Sightings.java
@@ -1,5 +1,8 @@
 // This template represents details recorded by rangers when they spot an animal
 package Models;
+import org.sql2o.Connection;
+
+import java.util.List;
 
 public class Sightings {
     private int animalId;
@@ -18,5 +21,23 @@ public class Sightings {
         this.ranger = ranger;
         this.location = location;
         this.animalId = animalId;
+    }
+
+    //save object
+    public void save() {
+        try(Connection con = DB.sql2o.open())  {
+            String sql = "INSERT INTO sightings (ranger, location, animalId) VALUES (:ranger, :location, :animalId)";
+            this.id = (int) con.createQuery(sql, true) .addParameter("ranger",this.ranger).addParameter("location", this.location).addParameter("animalId", this.animalId).executeUpdate().getKey();
+        }
+        catch (ClassCastException exc){
+        }
+    }
+
+    //retrieve records
+    public static List<Sightings> getAll() {
+        String sql = "SELECT * FROM sightings";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Sightings.class);/*all instances of Sightings*/
+        }
     }
 }
