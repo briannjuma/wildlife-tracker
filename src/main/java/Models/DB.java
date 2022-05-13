46ff214db4cb5f2d226e1e4bf62223ed4a7f041b
jumaBrian <njengabrianj@gmail.com>
2022-05-13 10:59:32 +0300

    debug: Fix database access

--- src/main/java/Models/DB.java
+++ src/main/java/Models/DB.java
@@ -3,9 +3,12 @@ package Models;
 import org.sql2o.Sql2o;
 
 public class DB {
-
+    // uncomment to use locally
     //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "njengabrayan","UBUNTU");
-    static String connectionString = "jdbc:postgresql://ec2-54-161-239-198.compute-1.amazonaws.com:5432/d444grvd7afi2t";
-    public static Sql2o sql2o = new Sql2o(connectionString,"fmgziuifsihprh","22d8b32aa822c86495478bfb5ed1c6f314179253de443e3ee21295e3fbeaae82");
 
+
+    // for use on deployed application
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-107-22-238-112.compute-1.amazonaws.com:5432/d1kgj8trcfoniq","ndzagjmiwxqfre","fc283c5a10f038e3e45e4fcbc032e1e6c18a9b19e1526783c993f7ab61ca69ce");
+
+    // uri -  postgres://ndzagjmiwxqfre:fc283c5a10f038e3e45e4fcbc032e1e6c18a9b19e1526783c993f7ab61ca69ce@ec2-107-22-238-112.compute-1.amazonaws.com:5432/d1kgj8trcfoniq
 }
